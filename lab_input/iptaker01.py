af82f84acbf42dc03e1f6fa53973f773515ced2e
m.zohaibhanif@gmail.com <student@bchd.2416dbd1-0fa3-4caf-962e-6e8dc3e165ab>
2022-11-07 19:56:52 +0000

    List images

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def main():
+    user_input = input("Please enter an IPv4 IP Address: ")
+
+    print("You told me the IPv4 address is: " + user_input)
+
+    vendor_name = input("What's the name of the vendor: ")
+
+    print("vendor name is: " + vendor_name)
+
+if __name__ == "__main__":
+    main()
