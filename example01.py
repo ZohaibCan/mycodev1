e35f5a53a447047faafd154adccf9a1211afc391
m.zohaibhanif@gmail.com <student@bchd.2416dbd1-0fa3-4caf-962e-6e8dc3e165ab>
2022-11-07 17:33:10 +0000

    example code push

---
+++ example01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+
+def main():
+
+
+    # display screen
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
---
+++ shebang.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def main():
+
+    print("ello!")
+    print("Did you say, hello?")
+    print("No, I said ello, but that\'s close enough.")
+
+if __name__ == "__main__":
+    main()
